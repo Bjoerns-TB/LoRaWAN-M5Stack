0926eb804faa05319a9596ab27e1feea8c7f3e03
Björn Amann <30447739+Bjoerns-TB@users.noreply.github.com>
2019-03-25 09:18:34 +0100

    Addded payload decoder

--- GPS.ino
+++ GPS.ino
@@ -1,4 +1,21 @@
+/* --- TTN Payload Decoder compatible with TTN Mapper integration
+function Decoder(b, port) {
 
+  var lat = (b[0] | b[1]<<8 | b[2]<<16 | (b[2] & 0x80 ? 0xFF<<24 : 0)) / 10000;
+  var lon = (b[3] | b[4]<<8 | b[5]<<16 | (b[5] & 0x80 ? 0xFF<<24 : 0)) / 10000;
+  var alt = (b[6] | b[7]<<8 | (b[7] & 0x80 ? 0xFF<<16 : 0)) / 100;
+  var hdop = b[8] / 10;
+
+  return {
+    
+      latitude: lat,
+      longitude: lon,
+      altitude: alt,
+      hdop: hdop
+    
+  };
+}
+*/
 
 #include <M5Stack.h>
 #include <TinyGPS++.h>
